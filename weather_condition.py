9cee56fbcabbc3820997fc9bc4227a0426e9f54c
shahriar <shahriarEbrampour@gmail.com>
2024-03-28 08:52:43 +0330

    Remove Get_Weather() due to 401 error

--- weather_condition.py
+++ weather_condition.py
@@ -1,7 +1,7 @@
 import requests
 
 # URL of the API endpoint
-url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Qazvin/2023-11-06/2024-02-22?unitGroup=metric&contentType=csv&include=days&key=XYAU3D5W3UQYBN3NSQSWVVXQ4"
+url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Qazvin/2023-11-06/2024-03-22?unitGroup=metric&contentType=csv&include=days&key=XYAU3D5W3UQYBN3NSQSWVVXQ4"
 
 # Send the GET request to the API
 def get_weather_history():
